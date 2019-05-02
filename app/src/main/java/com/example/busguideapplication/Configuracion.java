a615abb0c1fe2a0f15fd066efff630e0bcb7d88b
Beita <bei.g.g.97@gmail.com>
2019-05-02 11:55:36 +0100

    Permite cambiar contraseña, foto y nombre usuario

--- app/src/main/java/com/example/busguideapplication/Configuracion.java
+++ app/src/main/java/com/example/busguideapplication/Configuracion.java
@@ -3,6 +3,7 @@ package com.example.busguideapplication;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
@@ -18,35 +20,34 @@ import com.google.firebase.auth.UserProfileChangeRequest;
 public class Configuracion extends AppCompatActivity {
     private static final int CHOOSE_IMAGE = 101;
     private static final String TAG = "Configuracion";
-    EditText contraseña,Nombre;
-    Button cambiar,salir;
+    EditText Contraseña,Nombre;
     ImageView camarafotos;
 
     Uri uriprofile;
-    FirebaseAuth mAuth;
-    Integer aux;
+    String aux="1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.configuracion);
 
-        //contraseña = findViewById(R.id.contraseña);
+        Contraseña = findViewById(R.id.contraseña);
         Nombre = findViewById(R.id.Nombre);
-        if(!Nombre.equals(null)){
-            aux=1;
-        }
-        /*cambiar= findViewById(R.id.cambiar);
-        salir = findViewById(R.id.salir);
         camarafotos=findViewById(R.id.camarafotos);
-        */
+
         FirebaseAuth firebaseAuth= FirebaseAuth.getInstance();
         FirebaseUser user=firebaseAuth.getCurrentUser();
+        Log.i(TAG, "Usuario" + user.getEmail());
         if (user != null) {
             for (UserInfo profile : user.getProviderData()) {
                 String name = profile.getDisplayName();
+                Uri photoUrl=profile.getPhotoUrl();
                 if(name!=null){
                     Nombre.setText(name);
+                }
+
+                if(photoUrl!=null){
+                    camarafotos.setImageURI(photoUrl);
                 }
             }
         }
@@ -54,21 +55,53 @@ public class Configuracion extends AppCompatActivity {
     }
 
     public void Cambiar_Foto(View view){
-        //showImageChooser();
+        Intent gallery= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery,CHOOSE_IMAGE);
     }
 
     public void Salir(View view){
-        startActivity(new Intent(Configuracion.this,Ruta.class));
+        startActivity(new Intent(Configuracion.this,Inicio.class));
     }
 
     public void Cambiar_Datos(View view){
         FirebaseAuth firebaseAuth= FirebaseAuth.getInstance();
         FirebaseUser user=firebaseAuth.getCurrentUser();
-        String nombre_nuevo=Nombre.getText().toString();
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName(nombre_nuevo).build();
-        user.updateProfile(profileUpdates);
-        Log.i(TAG,"here"+ nombre_nuevo);
-        startActivity(new Intent(Configuracion.this,Inicio.class));
+        if(!Nombre.getText().toString().isEmpty()) {
+            String nombre_nuevo = Nombre.getText().toString();
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(nombre_nuevo).build();
+            user.updateProfile(profileUpdates);
+        }
+
+        if(!Contraseña.getText().toString().isEmpty()){
+            String contraseña_nueva=Contraseña.getText().toString().trim();
+            if(contraseña_nueva.length()< 6){
+                Toast.makeText(this, "La contraseña tiene que tener un tamaño superior o igual a 6. No se ha cambiado la contraseña", Toast.LENGTH_SHORT).show();
+            }else {
+                user.updatePassword(contraseña_nueva);
+                startActivity(new Intent(Configuracion.this,Inicio.class));
+                finish();
+            }
+        }else{
+            startActivity(new Intent(Configuracion.this,Inicio.class));
+        }
+
+        if(aux.equals("1")){
+            startActivity(new Intent(Configuracion.this,Inicio.class));
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode==RESULT_OK && requestCode==CHOOSE_IMAGE){
+            FirebaseAuth firebaseAuth= FirebaseAuth.getInstance();
+            FirebaseUser user=firebaseAuth.getCurrentUser();
+            uriprofile=data.getData();
+            camarafotos.setImageURI(uriprofile);
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setPhotoUri(uriprofile).build();
+            user.updateProfile(profileUpdates);
+            aux="1";
+        }
     }
 }
